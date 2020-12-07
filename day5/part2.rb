97f0753b12bae41e50a187c0c5ee0569f6217d00
Landon Schropp <schroppl@gmail.com>
2020-12-06 20:19:07 -0600

    Implement day 5 part 2

---
+++ day5/part2.rb
@@ -0,0 +1,16 @@
+require_relative "../shared/arguments.rb"
+
+Arguments.validate!(%w(file-path))
+
+def seat_id(seat_code)
+  row = seat_code[0..6].gsub(/./, { "F" => "0", "B" => "1" }).to_i(2)
+  column = seat_code[-3..].gsub(/./, { "L" => "0", "R" => "1" }).to_i(2)
+  row * 8 + column
+end
+
+puts IO
+  .read(ARGV[0])
+  .split("\n")
+  .map { seat_id(_1) }
+  .then { (0...(128 * 8)).to_a - _1 }
+  .then { |seats| seats.find { !seats.include?(_1 + 1) && !seats.include?(_1 - 1) } }
